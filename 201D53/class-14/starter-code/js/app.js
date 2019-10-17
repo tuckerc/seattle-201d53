7ff5c5ecfdc5dfa349f85f2549149b99fbcf01b0
Chase Tucker <tuck8106@gmail.com>
2019-10-17 12:58:46 -0700

    several app.js functions built

--- 201D53/class-14/starter-code/js/app.js
+++ 201D53/class-14/starter-code/js/app.js
@@ -8,15 +8,26 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  this.items.push(new CartItem(product, quantity));
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  if(supportsHTML5Storage()) {
+    // write to local products
+    localStorage.removeItem();
+    localStorage.setItem('classBusMall',JSON.stringify(this));
+    return true;
+  }
+  return false;
 };
 
-Cart.prototype.removeItem = function(item) {
+Cart.prototype.removeItem = function(name) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  if(this.items.name.includes(name)) {
+    this.items.splice(this.items.indexOf(name),1);
+  }
 };
 
 var CartItem = function(product, quantity) {
